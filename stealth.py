1b8d768c89acde7b9b3803d18f25fabfc6346123
jgreely <dotclue@github.fake>
2026-04-17 13:24:18 -0400

    added support for RGB stealth metadata, and for both types in PNG

--- stealth.py
+++ stealth.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 """
-quick standalone hack to extract SwarmUI stealth metadata from the
-alpha channel of WEBP images, where it's stored in the low bit.
-SwarmUI *can* also encode it in the RGB channels, as well as in PNG
-images, but the only person I know who's using it is Juan on the
-Discord, and he's using WEBP/alpha.
+
+Quick standalone hack to extract SwarmUI stealth metadata from the low
+bit in WEBP/PNG images, where it is stored in the alpha channel if
+present, or else the RGB channels. RGB is annoying to extract because
+it only works for *lossless* WEBP, and it's a continuous bitstream of
+3 bits per pixel, so it's a separate function to keep things clean.
+
+Note: encoding should be identical in PNG, but I haven't tested it.
 """
 
 import sys
 from PIL import Image
 import gzip
 
-def stealth_bytes(image, start=0, count=1):
+def stealth_bytes_alpha(image, start=0, count=1):
     """
     Convert the low bit of each pixel in the alpha channel into bytes,
     where pixel (x,y) through (x,y+7) contain the bits in order, high
@@ -39,9 +42,54 @@ def stealth_bytes(image, start=0, count=1):
         buf.append(b)
     return buf
 
-def webp_stealth_metadata(file):
+
+def stealth_bytes_rgb(image, start=0, count=1):
     """
-    If the file is a WEBP image with an alpha channel, attempt to
+    Convert the low bit of each channel in each pixel into bytes,
+    where pixel (x,y) through (x,y+2) contain the bits in order, high
+    to low, with one leftover to start the next byte. Because this
+    is a pain in the ass to decode at arbitrary offsets, it's easier
+    to just start from 0 each time and discard the unused bytes.
+    """
+    count *= 8
+    lowbits = list()
+    h = image.height
+    x = 0
+    y = 0
+    while len(lowbits) < start * 8 + count:
+        r,g,b = image.getpixel((x, y))
+        lowbits.append(r&1)
+        lowbits.append(g&1)
+        lowbits.append(b&1)
+        y += 1
+        if y == h:
+            y = 0
+            x += 1
+    buf = bytearray()
+    # discard leftover bits
+    while len(lowbits) % 8:
+        lowbits.pop()
+    for i in range(0, len(lowbits), 8):
+        b = 0
+        for j in range(8):
+            if i + j < len(lowbits):
+                b |= lowbits[i + j] << (7 - j)
+        buf.append(b)
+    if start > 0:
+        return buf[start:]
+    return buf
+
+
+def stealth_bytes(image, start, count):
+    if image.mode == 'RGBA':
+        return stealth_bytes_alpha(image, start, count)
+    else:
+        return stealth_bytes_rgb(image, start, count)
+
+
+def stealth_metadata(file):
+    """
+    If the file is a WEBP/PNG image with an alpha channel, attempt to
     extract SwarmUI stealth metadata from it.
     """
     try:
@@ -49,23 +97,23 @@ def webp_stealth_metadata(file):
     except Exception as e:
         print(e)
         sys.exit()
-    if im.format == 'WEBP' and im.mode == 'RGBA':
+    if im.format in ['WEBP', 'PNG']:
         magic = stealth_bytes(im, 0, 11).decode(errors='ignore')
-        if magic == 'stealth_png':
+        if magic in ['stealth_png', 'stealth_rgb']:
             is_comp = stealth_bytes(im, 11, 4).decode(errors='ignore')
             data_len = 0
             for b in stealth_bytes(im, 15, 4):
                 data_len = data_len * 256 + b
-            data = stealth_bytes(im, 19, data_len/8)
+            data = stealth_bytes(im, 19, data_len//8)
             if is_comp == 'comp':
                 data = gzip.decompress(data)
             params = data.decode(errors='ignore')
-            return(params)
+            return params
     return None
 
 sys.argv.pop(0)
 if len(sys.argv) > 0:
-    params = webp_stealth_metadata(sys.argv[0])
+    params = stealth_metadata(sys.argv[0])
     if params:
         print(params)
 else:
